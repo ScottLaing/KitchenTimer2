182c2728ac1a3c929e90221151a486b3c65ba01c
Scott Laing <scottlaing@gmail.com>
2024-04-16 17:47:21 -0500

    Add logging tracking questions for new logging features.

---
+++ KitchenTimer2/Entities/Log.cs
@@ -0,0 +1,20 @@
+﻿using KitchenTimer2.Enums;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KitchenTimer.Entities
+{
+    public class Log
+    {
+        public string? Name { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime? EndTime { get; set; }
+        public List<LogEntry>? Entries { get; set; } = new List<LogEntry>();
+        public string? Note { get; set; }
+        public LogTypes LogType { get; set; } = LogTypes.Other;
+    }
+}
---
+++ KitchenTimer2/Entities/LogEntry.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KitchenTimer.Entities
+{
+    public class LogEntry
+    {
+        public DateTime? TimeStamp { get; set; }
+        public TimeSpan? TimeDiff { get; set; }
+        public string? Note { get; set; }
+    }
+}
