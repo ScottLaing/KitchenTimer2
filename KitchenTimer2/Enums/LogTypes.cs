e502d2a173c09d24b17546db3e7b66c86d1f054f
Scott Laing <scottlaing@gmail.com>
2025-02-26 19:19:59 -0600

    Commenting adds.

--- KitchenTimer2/Enums/LogTypes.cs
+++ KitchenTimer2/Enums/LogTypes.cs
@@ -1,5 +1,8 @@
 ﻿namespace KitchenTimer2.Enums
 {
+    /// <summary>
+    /// Timer LogTypes, timer can be countdown timer, stopwatch, or other.
+    /// </summary>
     public enum LogTypes
     {
         Countdown,
